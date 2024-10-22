24248078fdfd4a0603ace2775122385dedf27ef0
Suyukee <diefuck@ya.ru>
2024-10-22 18:32:17 +0300

    Исправил ошибки

--- src/utils/fetching.ts
+++ src/utils/fetching.ts
@@ -1,10 +1,10 @@
-import { Data } from '@/types/recipes';
+import { DataDto } from '@/types/recipes';
 
 export async function getRecipes({ limit = 20, search = '' }) {
 	const res = await fetch(
 		`https://dummyjson.com/recipes/search?q=${search}&select=name,image,rating,prepTimeMinutes,cookTimeMinutes&limit=${limit}`,
 	);
-	const data: Data = await res.json();
+	const data: DataDto = await res.json();
 	return data;
 }
 
